5f4fd93217bd98f8673e17b09b7d90b03766b9bf
MonYamau <White.Fox1825@yandex.ru>
2026-04-07 21:39:08 +0500

    feat: add DAO method "create" by fields "code, fullName, sign"

--- src/main/java/com/project/currencyexchanger/Repository/CurrencyDAO.java
+++ src/main/java/com/project/currencyexchanger/Repository/CurrencyDAO.java
@@ -12,6 +12,8 @@ public class CurrencyDAO {
 
     private String sqlQueryAll = "SELECT * FROM CURRENCIES";
     private String sqlQuery = "SELECT * FROM CURRENCIES WHERE code = ?";
+    private String sqlQueryPost = "INSERT INTO CURRENCIES ('code', 'full_name', 'sign')" +
+            "VALUES (?, ?, ?)";
 
     public Optional<List> getCurrencies() {
         try (Connection con = DriverManager.getConnection(url);
@@ -55,5 +57,20 @@ public class CurrencyDAO {
             throw new RuntimeException(e);
         }
         return Optional.empty();
+    }
+
+    public int create(String code, String fullName, String sign) {
+        try (Connection con = DriverManager.getConnection(url);
+        PreparedStatement stmt = con.prepareStatement(sqlQueryPost)) {
+
+            stmt.setString(1, code);
+            stmt.setString(2, fullName);
+            stmt.setString(3, sign);
+
+            return stmt.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
