00dc76c73d8d215b39c684fa82d5cce7705bcce2
MonYamau <White.Fox1825@yandex.ru>
2026-04-10 17:51:12 +0500

    fix: correct name of class;

--- src/main/java/com/project/model/dto/exchangeRateDTO.java
+++
@@ -1,6 +0,0 @@
-package com.project.model.dto;
-
-import java.math.BigDecimal;
-
-public record exchangeRateDTO(Integer id, CurrencyDTO baseCurrency, CurrencyDTO targetCurrency, BigDecimal rate) {
-}
