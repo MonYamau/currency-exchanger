e65353e31b2dfacf1ab48fca803aac89299bcf40
MonYamau <White.Fox1825@yandex.ru>
2026-04-10 17:53:41 +0500

    feat: create class ExchangeRateService.java; add method to get all exchange rates;

---
+++ src/main/java/com/project/service/ExchangeRateService.java
@@ -0,0 +1,44 @@
+package com.project.service;
+
+import com.project.dao.ExchangeRateDAO;
+import com.project.model.Currency;
+import com.project.model.ExchangeRate;
+import com.project.model.dto.CurrencyDTO;
+import com.project.model.dto.ExchangeRateDTO;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class ExchangeRateService {
+    ExchangeRateDAO exchangeRateDAO = new ExchangeRateDAO();
+
+    public Optional<List<ExchangeRateDTO>> getAll() {
+        List<ExchangeRateDTO> result = new ArrayList<>();
+
+        Optional<List<ExchangeRate>> test = exchangeRateDAO.getExchangeRates();
+        if (test.isPresent()) {
+            for (ExchangeRate rate : test.get()) {
+
+                Currency baseCurrency = rate.getBaseCurrency();
+                Currency targetCurrency = rate.getTargetCurrency();
+
+                CurrencyDTO baseCurrencyDTO = new CurrencyDTO(
+                        baseCurrency.getId(), baseCurrency.getCode(),
+                        baseCurrency.getFullName(), baseCurrency.getSign());
+
+                CurrencyDTO targetCurrencyDTO = new CurrencyDTO(
+                        targetCurrency.getId(), targetCurrency.getCode(),
+                        targetCurrency.getFullName(), targetCurrency.getSign());
+
+                ExchangeRateDTO rateDTO = new ExchangeRateDTO(
+                        rate.getId(), baseCurrencyDTO, targetCurrencyDTO, BigDecimal.valueOf(rate.getRate()));
+
+                result.add(rateDTO);
+            }
+            return Optional.of(result);
+        }
+        return Optional.empty();
+    }
+}
