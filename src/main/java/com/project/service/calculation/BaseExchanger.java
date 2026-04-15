016903fa782b18de7c5de53fa862780aab9b0bef
MonYamau <White.Fox1825@yandex.ru>
2026-04-15 14:14:35 +0500

    feat: add baseExchanger for currency calculation based on the direct exchange rate;

---
+++ src/main/java/com/project/service/calculation/BaseExchanger.java
@@ -0,0 +1,20 @@
+package com.project.service.calculation;
+
+import com.project.model.ExchangeRate;
+
+import java.math.BigDecimal;
+import java.util.Optional;
+
+public class BaseExchanger extends Exchanger{
+
+    @Override
+    protected Optional<BigDecimal> getRate(String baseCode, String targetCode) {
+        Optional<ExchangeRate> check = exchangeRateDAO.getExchangeRate(baseCode, targetCode);
+        if (check.isPresent()) {
+            ExchangeRate exchangeRate = check.get();
+            BigDecimal rate = exchangeRate.getRate();
+            return Optional.of(rate);
+        }
+        return nextExchanger.getRate(baseCode, targetCode);
+    }
+}
