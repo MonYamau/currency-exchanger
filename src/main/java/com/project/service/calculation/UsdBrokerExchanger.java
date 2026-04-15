a3237095dd99a5781da638349afd8ef95e7b748f
MonYamau <White.Fox1825@yandex.ru>
2026-04-15 14:18:48 +0500

    feat: add usdBrokerExchanger for currency calculation based on the USD exchange rate;

---
+++ src/main/java/com/project/service/calculation/UsdBrokerExchanger.java
@@ -0,0 +1,25 @@
+package com.project.service.calculation;
+
+import com.project.model.ExchangeRate;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Optional;
+
+public class UsdBrokerExchanger extends Exchanger{
+    @Override
+    protected Optional<BigDecimal> getRate(String baseCode, String targetCode) {
+        String UsdCode = "USD";
+        Optional<ExchangeRate> baseCheck = exchangeRateDAO.getExchangeRate(UsdCode, baseCode);
+        Optional<ExchangeRate> targetCheck = exchangeRateDAO.getExchangeRate(UsdCode, targetCode);
+        if (baseCheck.isPresent() && targetCheck.isPresent()) {
+            ExchangeRate brokerForBaseCurrency = baseCheck.get();
+            ExchangeRate brokerForTargetCurrency = targetCheck.get();
+            BigDecimal baseBrokerRate = brokerForBaseCurrency.getRate();
+            BigDecimal targetBrokerRate = brokerForTargetCurrency.getRate();
+            BigDecimal rate = targetBrokerRate.divide(baseBrokerRate, 6, RoundingMode.HALF_EVEN);
+            return Optional.of(rate);
+        }
+        return Optional.empty();
+    }
+}
