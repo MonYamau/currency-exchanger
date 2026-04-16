6736fb3b2ae0e994a56864d27973e604b965f9e2
MonYamau <White.Fox1825@yandex.ru>
2026-04-16 18:56:38 +0500

    feat, style: add correct error handling for the "/exchange" endpoint, optimize code;

--- src/main/java/com/project/controller/ExchangerServlet.java
+++ src/main/java/com/project/controller/ExchangerServlet.java
@@ -1,6 +1,9 @@
 package com.project.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.project.exception.DataNotFoundException;
+import com.project.exception.DatabaseException;
+import com.project.exception.IncorrectInputException;
 import com.project.model.dto.ExchangeResultDTO;
 import com.project.service.ExchangerService;
 import jakarta.servlet.ServletException;
@@ -11,6 +14,7 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 import java.math.BigDecimal;
+import java.util.Map;
 import java.util.Optional;
 
 @WebServlet("/exchange")
@@ -21,21 +25,32 @@ public class ExchangerServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("application/json");
-        String baseCode = req.getParameter("from");
-        String targetCode = req.getParameter("to");
+        String baseCodeParam = req.getParameter("from");
+        String targetCodeParam = req.getParameter("to");
         String amountParam = req.getParameter("amount");
-        BigDecimal amount = new BigDecimal(amountParam);
-
         try {
-            Optional<ExchangeResultDTO> resultDTO = exchangerService.getResult(baseCode, targetCode, amount);
-            if (resultDTO.isPresent()) {
-                ExchangeResultDTO result = resultDTO.get();
-                String json = objectMapper.writeValueAsString(result);
-                resp.getWriter().write(json);
+            if (baseCodeParam.isEmpty() || targetCodeParam.isEmpty() || amountParam.isEmpty()) {
+                throw new IncorrectInputException("One of the fields is empty");
             }
-        } catch (Exception e) {
-            resp.setStatus(500);
-            resp.getWriter().write("Error: " + e.getMessage());
+            String baseCode = baseCodeParam.toUpperCase();
+            String targetCode = targetCodeParam.toUpperCase();
+            BigDecimal amount = new BigDecimal(amountParam);
+            ExchangeResultDTO result = exchangerService.getResult(baseCode, targetCode, amount);
+            String json = objectMapper.writeValueAsString(result);
+            resp.getWriter().write(json);
+        } catch (IncorrectInputException e) {
+            setException(resp, 400, e);
+        } catch (DataNotFoundException e) {
+            setException(resp, 404, e);
+        } catch (DatabaseException e) {
+            setException(resp, 500, e);
         }
+    }
+
+    private void setException(HttpServletResponse resp, int statusCode, Exception e) throws IOException {
+        resp.setStatus(statusCode);
+        Map<String, String> errorMsg = Map.of("message", e.getMessage());
+        String error = objectMapper.writeValueAsString(errorMsg);
+        resp.getWriter().write(error);
     }
 }
