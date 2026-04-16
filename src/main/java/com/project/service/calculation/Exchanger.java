89ddb1d277c89afe18128579e8567710877a4554
MonYamau <White.Fox1825@yandex.ru>
2026-04-16 18:53:05 +0500

    feat, style: add correct error handling for the "/exchange" endpoint, optimize code;

--- src/main/java/com/project/service/calculation/Exchanger.java
+++ src/main/java/com/project/service/calculation/Exchanger.java
@@ -1,6 +1,7 @@
 package com.project.service.calculation;
 
 import com.project.dao.ExchangeRateDAO;
+import com.project.exception.DataNotFoundException;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
@@ -14,15 +15,14 @@ public abstract class Exchanger {
         this.nextExchanger = exchanger;
     }
 
-    public Optional<BigDecimal> exchange(String baseCode, String targetCode, BigDecimal amount) {
+    public BigDecimal exchange(String baseCode, String targetCode, BigDecimal amount) {
         Optional<BigDecimal> rateCheck = getRate(baseCode, targetCode);
-        if (rateCheck.isPresent()) {
-            BigDecimal rate = rateCheck.get();
-            BigDecimal result = amount.multiply(rate);
-            BigDecimal scaledResult = result.setScale(6, RoundingMode.HALF_EVEN);
-            return Optional.of(scaledResult);
+        if (rateCheck.isEmpty()) {
+            throw new DataNotFoundException("Couldn't find the exchange rate");
         }
-        return Optional.empty();
+        BigDecimal rate = rateCheck.get();
+        BigDecimal result = amount.multiply(rate);
+        return result.setScale(6, RoundingMode.HALF_EVEN);
     }
 
     protected abstract Optional<BigDecimal> getRate(String baseCode, String targetCode);
