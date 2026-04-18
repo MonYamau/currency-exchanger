412787287ac7324c4748f03a375b51afebdfcb93
MonYamau <White.Fox1825@yandex.ru>
2026-04-18 20:05:14 +0500

    refactor: correct handler-method, change signature for "setError" method;

--- src/main/java/com/project/controller/BaseServlet.java
+++ src/main/java/com/project/controller/BaseServlet.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.project.dao.CurrencyDAO;
 import com.project.dao.ExchangeRateDAO;
 import com.project.exception.DataNotFoundException;
+import com.project.exception.DatabaseException;
 import com.project.exception.IncorrectInputException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletResponse;
@@ -24,23 +25,22 @@ public abstract class BaseServlet extends HttpServlet {
     }
 
     protected void handleError(HttpServletResponse resp, Exception e) throws IOException {
-        if (e instanceof NumberFormatException || e instanceof IncorrectInputException) {
-            setError(resp, 400, e);
-        }
-        if (e instanceof DataNotFoundException) {
-            setError(resp, 404, e);
-        }
-        if (e instanceof IllegalArgumentException) {
-            setError(resp, 409, e);
-        }
-        if (e != null) {
-            setError(resp, 500, e);
+        if (e instanceof IncorrectInputException) {
+            setError(resp, 400, e.getMessage());
+        } else if (e instanceof DataNotFoundException) {
+            setError(resp, 404, e.getMessage());
+        } else if (e instanceof IllegalArgumentException) {
+            setError(resp, 409, e.getMessage());
+        } else if (e instanceof DatabaseException) {
+            setError(resp, 500, e.getMessage());
+        } else if (e != null) {
+            setError(resp, 500, "Unknown server error");
         }
     }
 
-    protected void setError(HttpServletResponse resp, int statusCode, Exception e) throws IOException {
+    protected void setError(HttpServletResponse resp, int statusCode, String message) throws IOException {
         resp.setStatus(statusCode);
-        Map<String, String> errorMsg = Map.of("message", e.getMessage());
+        Map<String, String> errorMsg = Map.of("message", message);
         String error = objectMapper.writeValueAsString(errorMsg);
         resp.getWriter().write(error);
     }
