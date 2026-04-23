1014dff3f8b875880ddc6e60abf8ba0ef0341f46
MonYamau <White.Fox1825@yandex.ru>
2026-04-23 20:42:07 +0500

    feat: add dto for error, add filter;

--- src/main/java/com/project/controller/BaseServlet.java
+++ src/main/java/com/project/controller/BaseServlet.java
@@ -3,6 +3,7 @@ package com.project.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.project.dao.CurrencyDao;
 import com.project.dao.ExchangeRateDao;
+import com.project.dto.ErrorDto;
 import com.project.exception.AlreadyExistsException;
 import com.project.exception.DataNotFoundException;
 import com.project.exception.DatabaseException;
@@ -11,7 +12,6 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
-import java.util.Map;
 
 public abstract class BaseServlet extends HttpServlet {
     protected static final ObjectMapper objectMapper = new ObjectMapper();
@@ -19,8 +19,6 @@ public abstract class BaseServlet extends HttpServlet {
     protected ExchangeRateDao exchangeRateDao = new ExchangeRateDao();
 
     protected void sendResultResponse(HttpServletResponse resp, int statusCode, Object body) throws IOException {
-        resp.setContentType("application/json");
-        resp.setCharacterEncoding("UTF-8");
         resp.setStatus(statusCode);
         String json = objectMapper.writeValueAsString(body);
         resp.getWriter().write(json);
@@ -42,8 +40,8 @@ public abstract class BaseServlet extends HttpServlet {
 
     private void sendErrorResponse(HttpServletResponse resp, int statusCode, String message) throws IOException {
         resp.setStatus(statusCode);
-        Map<String, String> errorMsg = Map.of("message", message);
-        String error = objectMapper.writeValueAsString(errorMsg);
+        ErrorDto errorDto = new ErrorDto(message);
+        String error = objectMapper.writeValueAsString(errorDto);
         resp.getWriter().write(error);
     }
 }
---
+++ src/main/java/com/project/filter/ServletFilter.java
@@ -0,0 +1,17 @@
+package com.project.filter;
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+
+import java.io.IOException;
+
+@WebFilter("/*")
+public class ServletFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain filterChain) throws IOException, ServletException {
+        resp.setContentType("application/json");
+        resp.setCharacterEncoding("UTF-8");
+        filterChain.doFilter(req, resp);
+    }
+}
