78402adaa11292963261bd180d625b0f957f9f56
MonYamau <White.Fox1825@yandex.ru>
2026-04-23 19:52:18 +0500

    refactor: implement mapper;

--- src/main/java/com/project/service/ExchangerService.java
+++ src/main/java/com/project/service/ExchangerService.java
@@ -5,6 +5,7 @@ import com.project.dao.ExchangeRateDao;
 import com.project.dto.ConversionResultDto;
 import com.project.dto.CurrencyDto;
 import com.project.exception.DataNotFoundException;
+import com.project.mapper.CurrencyMapper;
 import com.project.model.Currency;
 import com.project.provider.ExchangeRateProvider;
 
@@ -14,6 +15,8 @@ import java.util.Optional;
 
 public class ExchangerService {
     private static final int CONVERTED_AMOUNT_ROUNDING = 2;
+
+    private final CurrencyMapper mapper = CurrencyMapper.INSTANCE;
 
     CurrencyDao currencyDao;
     ExchangeRateDao exchangeRateDao;
@@ -26,25 +29,21 @@ public class ExchangerService {
     }
 
     public ConversionResultDto getConversion(String baseCode, String targetCode, BigDecimal amount) {
-        Currency baseCurrency = findCurrency(baseCode);
-        Currency targetCurrency = findCurrency(targetCode);
-        CurrencyDto baseCurrencyDto = getDto(baseCurrency);
-        CurrencyDto targetCurrencyDto = getDto(targetCurrency);
+        Currency baseCurrency = get(baseCode);
+        Currency targetCurrency = get(targetCode);
+        CurrencyDto baseCurrencyDto = mapper.toDto(baseCurrency);
+        CurrencyDto targetCurrencyDto = mapper.toDto(targetCurrency);
         BigDecimal rate = findRate(baseCode, targetCode);
         BigDecimal convertedAmount = exchange(rate, amount);
         return new ConversionResultDto(baseCurrencyDto, targetCurrencyDto, rate, amount, convertedAmount);
     }
 
-    private Currency findCurrency(String code) {
+    private Currency get(String code) {
         Optional<Currency> currencyCheck = currencyDao.get(code);
         if (currencyCheck.isEmpty()) {
             throw new DataNotFoundException("Couldn't find the currency with the " + code + " code");
         }
         return currencyCheck.get();
-    }
-
-    private CurrencyDto getDto(Currency currency) {
-        return new CurrencyDto(currency.getId(), currency.getCode(), currency.getFullName(), currency.getSign());
     }
 
     private BigDecimal findRate(String baseCode, String targetCode) {
