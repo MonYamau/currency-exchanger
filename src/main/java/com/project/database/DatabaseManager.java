6ad9766f6b42952fdbc15371b72ae0f44234fc17
MonYamau <White.Fox1825@yandex.ru>
2026-04-24 21:00:56 +0500

    feat: create interface, rename and change DatabaseManager for SQLite;

--- src/main/java/com/project/database/DatabaseManager.java
+++ src/main/java/com/project/database/DatabaseManager.java
@@ -1,35 +1,8 @@
 package com.project.database;
 
-import com.project.exception.DatabaseException;
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
-
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.SQLException;
 
-public class DatabaseManager {
-    private static final int MAX_POOL_SIZE_NUM = 5;
-    private static final int CONNECTION_TIMEOUT_MS = 5000;
-    private static final DataSource dataSource;
-
-    static {
-        try {
-            Class.forName("org.sqlite.JDBC");
-        } catch (ClassNotFoundException e) {
-            throw new DatabaseException("Couldn't load the driver: " + e.getMessage());
-        }
-        HikariConfig config = new HikariConfig();
-        config.setJdbcUrl("jdbc:sqlite:C:/SQLiteDatabase/currency_exchanger.db?" +
-                "journal_mode=WAL&busy_timeout=2000");
-        config.setMaximumPoolSize(MAX_POOL_SIZE_NUM);
-        config.setConnectionTimeout(CONNECTION_TIMEOUT_MS);
-        dataSource = new HikariDataSource(config);
-    }
-
-    private DatabaseManager () {}
-
-    public static Connection getConnection() throws SQLException {
-        return dataSource.getConnection();
-    }
+public interface DatabaseManager {
+    DataSource getDataSource();
+    void close();
 }
---
+++ src/main/java/com/project/database/SQLiteDatabaseManager.java
@@ -0,0 +1,40 @@
+package com.project.database;
+
+import com.project.exception.DatabaseException;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class SQLiteDatabaseManager implements DatabaseManager {
+    private static final int MAX_POOL_SIZE_NUM = 5;
+    private static final int CONNECTION_TIMEOUT_MS = 5000;
+    private final HikariDataSource dataSource;
+
+    public SQLiteDatabaseManager() {
+        try {
+            Class.forName("org.sqlite.JDBC");
+        } catch (ClassNotFoundException e) {
+            throw new DatabaseException("Couldn't load the driver: " + e.getMessage());
+        }
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl("jdbc:sqlite:C:/SQLiteDatabase/currency_exchanger.db?" +
+                "journal_mode=WAL&busy_timeout=2000");
+        config.setMaximumPoolSize(MAX_POOL_SIZE_NUM);
+        config.setConnectionTimeout(CONNECTION_TIMEOUT_MS);
+        this.dataSource = new HikariDataSource(config);
+    }
+
+    public DataSource getDataSource() {
+        return dataSource;
+    }
+
+    @Override
+    public void close() {
+        if (dataSource != null) {
+            dataSource.close();
+        }
+    }
+}
