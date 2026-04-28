a18c7356eccd2e8b131fa55619d3dc1a1a8df309
MonYamau <White.Fox1825@yandex.ru>
2026-04-28 19:49:43 +0500

    style: use reformat code;

--- src/main/resources/initialization.sql
+++ src/main/resources/initialization.sql
@@ -25,18 +25,18 @@ create unique index unique_pair
     on exchange_rates (base_currency_id, target_currency_id);
 
 insert into currencies (code, full_name, sign)
-values  ('USD', 'US Dollar', '$'),
-        ('AUD', 'Australian Dollar', 'A$'),
-        ('EUR', 'Euro', '€'),
-        ('RUB', 'Russian Ruble', '₽'),
-        ('XOF', 'CFA Franc BCEAO', '₣'),
-        ('JPY', 'Yen', '¥'),
-        ('XCD', 'Eastern Caribbean Dollar', 'EC$');
+values ('USD', 'US Dollar', '$'),
+       ('AUD', 'Australian Dollar', 'A$'),
+       ('EUR', 'Euro', '€'),
+       ('RUB', 'Russian Ruble', '₽'),
+       ('XOF', 'CFA Franc BCEAO', '₣'),
+       ('JPY', 'Yen', '¥'),
+       ('XCD', 'Eastern Caribbean Dollar', 'EC$');
 
 insert into exchange_rates (base_currency_id, target_currency_id, rate)
-values  ( 1, 3, 0.8532),
-        (1, 4, 75.53),
-        (1, 2, 1.4),
-        (2, 3, 0.6102),
-        (5, 4, 0.1349),
-        (1, 5, 559.82);
+values (1, 3, 0.8532),
+       (1, 4, 75.53),
+       (1, 2, 1.4),
+       (2, 3, 0.6102),
+       (5, 4, 0.1349),
+       (1, 5, 559.82);
